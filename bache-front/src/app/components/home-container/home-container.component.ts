d8b2015526a5460842856798b11d821f192fc50f
Fs <fede.stulich@gmail.com>
2020-01-27 22:41:35 -0300

    tenemos la segunda pagina

--- bache-front/src/app/components/home-container/home-container.component.ts
+++ bache-front/src/app/components/home-container/home-container.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-container',
   templateUrl: './home-container.component.html',
-  styleUrls: ['./home-container.component.sass']
+  styleUrls: ['./home-container.component.scss']
 })
 export class HomeContainerComponent implements OnInit {
+
+  isLogin: boolean = true;
+  isRegister: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  } 
+
+  onDisplayLogin(): void {
+    this.isLogin = true;
+    this.isRegister = false;
+  }
+
+  onDisplayRegister(): void {
+    this.isLogin = false;
+    this.isRegister = true;
   }
 
 }
